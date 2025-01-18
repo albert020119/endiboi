46039543b38aa857d0e03e4c3ace9538d7bf3ead
Albert Endre <albert_endre@yahoo.com>
2025-01-18 17:31:09 +0200

    modified projects page

--- endiboi/src/pages/ProjectsPage.js
+++ endiboi/src/pages/ProjectsPage.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import {AnimatedBackground} from 'animated-backgrounds'; 
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import { AnimatedBackground } from "animated-backgrounds";
+import "../styles.css";
 
 const projects = [
   {
-    title: 'APK Analyzer',
+    title: "APK Analyzer",
     description:
-      'My diploma work. I essentially created an Anti Virus for Android. Aside from the standard boring static analysis, the project uses Frida hooking to perform dynamic analysis on downloaded APK files on the users device.',
-    images: ['frida.png'],
-    techStack: ['Python', 'FastAPI', 'Java', 'JavaScript', 'Emulators', 'Android OS'],
+      "My diploma work. I essentially created an Anti Virus for Android. Aside from the standard boring static analysis, the project uses Frida hooking to perform dynamic analysis on downloaded APK files on the user's device.",
+    images: ["frida.png"],
+    techStack: ["Python", "FastAPI", "Java", "JavaScript", "Emulators", "Android OS"],
   },
   {
-    title: 'qESTkit',
-    description: 'A quantum circuit simulation library, created together with a few colleagues. We essentially created a fully fledged quantum simulator and ran Grover on it.',
-    images: ['quantum.png'],
-    techStack: ['Python', 'Quantum Computing', 'Matplotlib', 'Numpy', 'QKD'],
+    title: "qESTkit",
+    description:
+      "A quantum circuit simulation library, created together with a few colleagues. We essentially created a fully fledged quantum simulator and ran Grover on it",
+    images: ["quantum.png"],
+    techStack: ["Python", "Quantum Computing", "Matplotlib", "Numpy", "QKD"],
   },
   {
-    title: 'Rust Type Racer',
-    description: 'A command line typeracer. Creating this was an excuse to learn Rust',
-    images: ['rust.png'],
-    techStack: ['Rust', 'Command line', 'TTY'],
+    title: "Rust Type Racer",
+    description: "A command line typeracer. Creating this was an excuse to learn Rust",
+    images: ["rust.png"],
+    techStack: ["Rust", "Command line", "TTY"],
   },
 ];
 
@@ -38,25 +39,32 @@ const ProjectsPage = () => {
 
   return (
     <div>
-    <AnimatedBackground animationName="particleNetwork" blendMode="normal"/>"    
-    <div className="projects-container">
-      {visibleProjects.map((project, index) => (
-        <div key={index} className="project-section">
-          <div className="project-image-container">
-            <img src={project.images[0]} alt={`Project ${index + 1}`} />
-          </div>
-          <div className="project-content">
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-            <div className="project-tech-stack">
-              {project.techStack.map((tech, techIndex) => (
-                <span key={techIndex}>{tech}</span>
-              ))}
+      <AnimatedBackground animationName="particleNetwork" blendMode="normal" />
+      <div className="projects-container">
+        {visibleProjects.map((project, index) => (
+          <div key={index} className="project-card">
+            <img
+              src={project.images[0]}
+              alt={`Project ${index + 1}`}
+              className="project-image-container"
+            />
+            <div className="project-details">
+                <div className="project-text">
+                  <h2>{project.title}</h2>
+                  <p>{project.description}</p>
+                </div>
+                <div className="project-tech-stack">
+                  {project.techStack.map((tech, techIndex) => (
+                    <span key={techIndex} className="tech-item">
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 };
